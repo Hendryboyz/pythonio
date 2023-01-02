e308af85d04d7529b15e6de76654c425671fe673
Hendryboyz <hendry.js1247@gmail.com>
2023-01-02 23:14:21 +0800

    feat: start pipe client in process

--- pythonio/main.py
+++ pythonio/main.py
@@ -1,25 +1,25 @@
 from pythonio.key_server import KeyServer
 from pythonio.websocket_listener import WebsocketListener
-# from pythonio.pipe_listener import PipeListener
+from pythonio.pipe_listener import PipeListener
 # from pythonio.shared_memory_listener import SharedMemoryListener
 
 def main():
   client1 = WebsocketListener('websocket')
-  # client2 = PipeListener('pipe')
+  client2 = PipeListener('pipe')
   # client3 = SharedMemoryListener('shared-memory')
   
   p1 = client1.start()
-  # p2 = client2.start()
+  p2 = client2.start()
   # p3 = client3.start()
   
   server = KeyServer()
   server.add(client1)
-  # server.add(client2)
+  server.add(client2)
   # server.add(client3)
   server.start()
   
   p1.join()
-  # p2.join()
+  p2.join()
   # p3.join()
   
 
---
+++ pythonio/pipe_listener.py
@@ -0,0 +1,29 @@
+from pythonio.observers.interface import Listener, Server
+from multiprocessing.process import BaseProcess
+from multiprocessing.connection import Connection
+from multiprocessing import Process, Pipe
+
+def run_client_2(conn: Connection):
+  print('Client2 is ready')
+  while True:
+    nums = conn.recv()
+    print(f'Client 2 receive: {nums}')
+
+class PipeListener(Listener):
+  __conn: Connection = None
+  
+  def __init__(self, name: str) -> None:
+    super().__init__(name)
+    
+  def start(self) -> BaseProcess:
+    super().start()
+    receiver_conn, sender_conn = Pipe()
+    self.__conn = sender_conn
+    p = Process(target=run_client_2, args=(receiver_conn,))
+    p.start()
+    return p
+    
+  def calculate(self, server: Server) -> None:
+    super().calculate(server)
+    self.__conn.send(server._nums)
+    
