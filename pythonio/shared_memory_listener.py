96875b10faee74ba7c65db103f18a53f9dfcf26a
Hendryboyz <hendry.js1247@gmail.com>
2023-01-03 13:46:01 +0800

    feat: recieve number list from proxy list(shared memory)

--- pythonio/shared_memory_listener.py
+++ pythonio/shared_memory_listener.py
@@ -1,22 +1,37 @@
 from pythonio.observers.interface import Listener, Server
 from multiprocessing.process import BaseProcess
 from multiprocessing.sharedctypes import SynchronizedArray
-from multiprocessing import Process, Array
+from multiprocessing.managers import ListProxy
+from multiprocessing import Process, Array, Manager
+from time import sleep
 
 
-def run_client_3(nums: SynchronizedArray):
+def mode_client(nums: ListProxy):
   print('Client3 is ready')
+  while True:
+    sleep(1)
+    if len(nums) > 0:
+      print(nums)
+      nums[:] = []
+    else:
+      continue
 
 class SharedMemoryListener(Listener):
+  __shared_nums: ListProxy = None
+  
   def __init__(self, name: str) -> None:
     super().__init__(name)
     
   def start(self) -> BaseProcess:
     super().start()
-    nums = Array('i', ())
-    p = Process(target=run_client_3, args=(nums,))
+    manager = Manager()
+    self.__shared_nums = manager.list([])
+    p = Process(target=mode_client, args=(self.__shared_nums,))
     p.start()
     return p
     
   def calculate(self, server: Server) -> None:
     super().calculate(server)
+    self.__shared_nums[:] = []
+    for num in server._nums:
+      self.__shared_nums.append(num)
