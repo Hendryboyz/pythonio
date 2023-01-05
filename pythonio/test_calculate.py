f70a589cfac87743dbab030b36fe3da86258c39c
Hendryboyz <Hendry.js1247@gmail.com>
2023-01-05 10:33:09 +0800

    chore: avoid testing mode with order issues

--- pythonio/test_calculate.py
+++ pythonio/test_calculate.py
@@ -1,5 +1,4 @@
 import pytest
-import unittest
 from pythonio.utils.calculate import mean, mode
 @pytest.fixture(scope='function', params=(
   ([1, 2, 3], 2),
@@ -10,20 +9,18 @@ def mean_fixture(request):
 
 @pytest.fixture(scope='function', params=(
   ([9, 10, 12, 13, 13, 13, 15, 15, 16, 16, 18, 22, 23, 24, 24, 25], [13]),
-  ([2, 1, 2, 1], [1, 2]),
+  ([1, 1, 2, 2], [1, 2]),
 ))
 def mode_fixture(request):
   return request.param
 
 class TestCaculator:
   
-  @pytest.mark.usefixtures('mode_fixture')
   def test_mode(self, mode_fixture):
     nums, expect = mode_fixture
     actual = mode(nums)
     assert expect == actual
 
-  # @pytest.mark.usefixtures('mean_fixture')
   def test_mean(self, mean_fixture):
     nums, expect = mean_fixture
     actual = mean(nums)
